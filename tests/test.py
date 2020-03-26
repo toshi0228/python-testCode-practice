6ee82c0da2eb48cc7e169520439057f945106f4f
toshi <etoshi0228@gmail.com>
2020-03-26 22:10:07 +0900

    test1

---
+++ tests/test.py
@@ -0,0 +1,14 @@
+import unittest
+
+
+def plus(a, b):
+    return a + b
+
+
+class TestPlus(unittest.TestCase):
+
+    def test_Plus(self):
+        self.assertEqual(10, plus(2, 8))
+
+
+# python -m unittest discover tests/
